ca8db5159063d5343f93a6e69ae744d132364d9b
mamalekb <57837750+mamalekb@users.noreply.github.com>
2019-11-16 16:33:42 +0100

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
-System.out.printl("This world is not enough!")
+        System.out.printl("This world is not enough!")
     }
 
 }
